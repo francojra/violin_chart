cef041fb81e25e66b837303cd45e537f76d65be2
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-10-07 23:21:50 -0300

    Add files via upload

---
+++ violin_chart.R
@@ -0,0 +1,13 @@
+
+# Gráfico Violino --------------------------------------------------------------------------------------------------------------------------
+# Autora do script: Jeanne Franco ----------------------------------------------------------------------------------------------------------
+# Data: 07/10/23 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://r-graph-gallery.com/violin.html --------------------------------------------------------------------------------------
+
+# Introdução -------------------------------------------------------------------------------------------------------------------------------
+
+### Gráficos violino permitem visualizar a distribuição de uma variável numérica para
+### um ou vários grupos. Eles são muito bem adaptados grandes conjuntos de dados. Eles
+### são frequentemente usados para substituir o boxplot. A função usada para gerar esse
+### gráfico com ggplot2 é geom_violin.
+
